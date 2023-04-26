3aeae757ff8011500587f57f25f8f79d9e40b4bd
Joshua Jackson (Yuppiefab) <masterjack612@gmail.com>
2023-04-26 08:51:16 -0700

    print_rot.c

--- print_rot.c
+++ print_rot.c
@@ -20,7 +20,7 @@ int print_rot(va_list arguments, char *buf, unsigned int ibuf)
 	if (str == NULL)
 	{
 		for (i = 0; nill[i]; i++)
-			ibuf = handl_buf(buf, nill[i], ibuf);
+			ibuf = hand1_buf(buf, nill[i], ibuf);
 		return (6);
 	}
 	for (i = 0; str[i]; i++)
@@ -30,12 +30,12 @@ int print_rot(va_list arguments, char *buf, unsigned int ibuf)
 			if (str[i] == alf[j])
 			{
 				k = 1;
-				ibuf = handl_buf(buf, rot[j], ibuf);
+				ibuf = hand1_buf(buf, rot[j], ibuf);
 				break;
 			}
 		}
 		if (k == 0)
-			ibuf = handl_buf(buf, str[i], ibuf);
+			ibuf = hand1_buf(buf, str[i], ibuf);
 	}
 	return (i);
 }
