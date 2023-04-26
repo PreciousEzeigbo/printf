782147ff3662cbe180c64fc0c74edd765b02206e
Joshua Jackson (Yuppiefab) <masterjack612@gmail.com>
2023-04-26 05:09:31 -0700

    print long

--- print_long_upx.c
+++ print_long_upx.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * prinlupx - prints a long decimal in hexadecimal
+ * printing_upx - prints a long decimal in hexadecimal
  * @arguments: The character to print
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
-int prinlupx(va_list arguments, char *buf, unsigned int ibuf)
+int printing_upx(va_list arguments, char *buf, unsigned int ibuf)
 {
 	long int int_input, i, isnegative, count, first_digit;
 	char *hexadecimal, *binary;
